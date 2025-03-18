46af753d327fcfb2ea6b372ff800429a86bfca5b
yeeun426 <thsudkcla7@naver.com>
2025-03-18 19:58:20 +0900

    feat : kakao OAuth

---
+++ Ycarback/src/main/java/com/back/ycar/controller/OAuthController.java
@@ -0,0 +1,53 @@
+package com.back.ycar.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.back.ycar.service.OAuthService;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class OAuthController {
+
+	@Value("${kakao.api.key}")
+	String KAKAO_API_KEY ;
+	
+	@Autowired
+	OAuthService oAuthService;
+	
+	@GetMapping("kakaoLogin")
+	public String kakaoLogin() {
+		
+		return "redirect:https://kauth.kakao.com/oauth/authorize?client_id=" 
+		+ KAKAO_API_KEY 
+				+ "&redirect_uri=http://localhost:8080/kakaoLoginCallback&response_type=code";
+	}
+	
+	@GetMapping("kakaoLoginCallback")
+	public String kakaoCallback(@RequestParam String code, HttpServletRequest request, HttpServletResponse response) throws Exception{
+	  System.out.println("사용자 동의 코드:" + code);
+	  String access_Token = oAuthService.getKaKaoAccessToken(code);
+	  System.out.println("access_Token :" + access_Token );
+	  String email = oAuthService.getKakaoUser(access_Token);
+	  System.out.println("email  :" + email  );
+
+	  if (email != null) {
+		// db에 저장
+		oAuthService.insertLoginInfo(email, access_Token);
+			
+		Cookie c1 = new Cookie("email", email);
+		Cookie c2 = new Cookie("Authorization", access_Token);
+		
+		response.addCookie(c1);
+		response.addCookie(c2);
+
+	}
+		return "redirect:http://localhost:3000/";
+	}
+}
---
+++ Ycarback/src/main/java/com/back/ycar/service/OAuthService.java
@@ -0,0 +1,138 @@
+package com.back.ycar.service;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.back.ycar.dao.LoginDao;
+import com.back.ycar.dto.Login;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class OAuthService {
+	@Value("${kakao.api.key}")
+	String KAKAO_API_KEY ;
+	
+	@Autowired
+	LoginDao loginDao;
+	
+	public void insertLoginInfo(String email, String kakaoToken) throws Exception {
+		Login loginInfo=new Login(email, kakaoToken, new Date());
+		loginDao.insertToken(loginInfo);
+	}
+
+	public String getKaKaoAccessToken(String code) {
+	    String access_Token = "";
+	    String refresh_Token = "";
+	    String reqURL = "https://kauth.kakao.com/oauth/token";
+	   
+	    try {
+	        URL url = new URL(reqURL);
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+	        // POST 요청을 위해 기본값이 false인 setDoOutput을 true로
+	        conn.setRequestMethod("POST");
+	        conn.setDoOutput(true);
+
+	        // POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
+	        StringBuilder sb = new StringBuilder();
+	        sb.append("grant_type=authorization_code");
+	        sb.append("&client_id=" + KAKAO_API_KEY );
+	        sb.append("&prompt=login");
+	        sb.append("&redirect_uri=http://localhost:8080/kakaoLoginCallback");
+	        sb.append("&code=" + code);
+	        
+	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+	        bw.write(sb.toString());
+	        bw.flush();
+
+	        // 결과 코드가 200이라면 성공
+	        int responseCode = conn.getResponseCode();
+	        System.out.println("responseCode : " + responseCode);
+	        
+	        // 요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+	        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	        
+	        String line = "";
+	        String result = "";
+
+	        while ((line = br.readLine()) != null) {
+	            result += line;
+	        }
+	        System.out.println("response body : " + result);
+
+	        // Jackson 라이브러리로 JSON 파싱
+	        ObjectMapper objectMapper = new ObjectMapper();
+	        JsonNode jsonNode = objectMapper.readTree(result);
+
+	        access_Token = jsonNode.get("access_token").asText();
+	        refresh_Token = jsonNode.get("refresh_token").asText();
+
+	        System.out.println("access_token : " + access_Token);
+	        System.out.println("refresh_token : " + refresh_Token);
+
+	        br.close();
+	        bw.close();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+
+	    return access_Token;
+	}	
+	
+	public String getKakaoUser(String token) {
+	    String reqURL = "https://kapi.kakao.com/v2/user/me";
+
+	    // access_token을 이용하여 사용자 정보 조회
+	    try {
+	        URL url = new URL(reqURL);
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+	        conn.setRequestMethod("POST");
+	        conn.setDoOutput(true);
+	        conn.setRequestProperty("Authorization", "Bearer " + token);
+
+	        // 결과 코드가 200이라면 성공
+	        int responseCode = conn.getResponseCode();
+	        System.out.println("responseCode : " + responseCode);
+
+	        // 요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+	        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	        String line = "";
+	        String result = "";
+
+	        while ((line = br.readLine()) != null) {
+	            result += line;
+	        }
+	        System.out.println("response body : " + result);
+
+	        // Jackson 라이브러리로 JSON 파싱
+	        ObjectMapper objectMapper = new ObjectMapper();
+	        JsonNode jsonNode = objectMapper.readTree(result);
+
+	        int id = jsonNode.get("id").asInt();
+	        boolean hasEmail = jsonNode.get("kakao_account").get("has_email").asBoolean();
+	        String email = "";
+	        if (hasEmail) {
+	            email = jsonNode.get("kakao_account").get("email").asText();
+	        }
+
+	        System.out.println("id : " + id);
+	        System.out.println("email : " + email);
+
+	        br.close();
+
+	        return email;
+
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	        return null;
+	    }
+	}	
+}
