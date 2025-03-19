99cb6a798dc577e8f96eac0a20b0fdf1785c5a69
yeeun426 <thsudkcla7@naver.com>
2025-03-20 00:42:28 +0900

    feat : 차량 추가 - redis test

--- Ycarback/src/main/java/com/back/ycar/service/CarService.java
+++ Ycarback/src/main/java/com/back/ycar/service/CarService.java
@@ -1,20 +1,38 @@
 package com.back.ycar.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.back.ycar.dao.CarDao;
+import com.back.ycar.dao.UsedCarDao;
 import com.back.ycar.dto.Car;
+import com.back.ycar.dto.UsedCar;
 
 @Service
 public class CarService {
     
     @Autowired
     CarDao carDao;
+    @Autowired
+    UsedCarDao usedCarDao;
+
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
     
+    private static final String USED_CARS_KEY = "usedCars";
+
     public void registerCar(Car car) throws Exception {
         // 등록 로직 (필요에 따라 트랜잭션 처리 등 추가)
         carDao.insertCar(car);
+        System.out.println("SQL 등록");
+        
+        // Redis 캐시 갱신
+        List<UsedCar> updatedCars = usedCarDao.getAllUsedCars();
+        redisTemplate.opsForValue().set(USED_CARS_KEY, updatedCars);
+        System.out.println("Redis 캐시 갱신 완료");
     }
 }
-
